2496cdcf9443eed9522ebc93af62b9fd48108543
furkanpur <furkanpur@gmail.com>
2015-05-03 00:17:18 +0300

    Last Update

---
+++ lcd_display/code/code.ino
@@ -0,0 +1,27 @@
+#include <Wire.h>
+
+#include <FastIO.h>
+#include <I2CIO.h>
+#include <LCD.h>
+#include <LiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
+#include <LiquidCrystal_SR.h>
+#include <LiquidCrystal_SR2W.h>
+#include <LiquidCrystal_SR3W.h>
+
+
+
+
+LiquidCrystal_I2C lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+ 
+void setup()
+{
+ lcd.begin (16,2);
+}
+ 
+void loop()
+{
+  lcd.setBacklight(HIGH);
+  lcd.setCursor(0,1);
+  lcd.print("HELLO WORLD :)");
+}
